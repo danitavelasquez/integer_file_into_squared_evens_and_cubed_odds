1bb7c8917974051443d731b293fd21657d22e6b0
danitavelasquez <danitavelasquez7002@gmail.com>
2026-04-29 22:40:00 +0800

    Code now OOP and remade changes

--- spits_integers_to_squaredeven_and_cubedodd.py
+++ spits_integers_to_squaredeven_and_cubedodd.py
@@ -1,32 +1,45 @@
-# defines the function, also reads a file
-def integer():
-    integers = open("integers.txt", "r")
-    # creates list
-    even = []
-    odd = []
-    # splits linea
-    for line in integers:
-        numbers = line.split()
+# creates class
+class IntegerProcessor:
+    def __init__(self):
+        # input and output file names
+        self.input_file = "integers.txt"
+        self.even_file = "double.txt"
+        self.odd_file = "triple.txt"
 
-        for n in numbers:
-            n = int(n)
-            # checks if even then squares it
-            if n % 2 == 0:
-                even.append(n ** 2)
-            # checks if odd then cubes it
-            else:
-                odd.append(n ** 3)
+    # process the numbers and opens file
+    def process_numbers(self):
+        integers = open(self.input_file, "r")
+        # creates list
+        even = []
+        odd = []
+
+        # splits lines
+        for line in integers:
+            numbers = line.split()
+
+            for n in numbers:
+                n = int(n)
+                # checks if even then squares it
+                if n % 2 == 0:
+                    even.append(n ** 2)
+                # checks if odd then cubes it
+                else:
+                    odd.append(n ** 3)
+
+        integers.close()
 
         # writes a file containing all the doubled even integers
-        double_even = open("double.txt", "w")
+        double_even = open(self.even_file, "w")
         for num in even:
             double_even.write(str(num) + "\n")
         double_even.close()
         # writes a file containing all the cubed odd integers
-        triple_odd = open("triple.txt", "w")
+        triple_odd = open(self.odd_file, "w")
         for num in odd:
             triple_odd.write(str(num) + "\n")
         triple_odd.close()
 
-    integers.close()
-integer()
+
+# creates object and runs
+processor = IntegerProcessor()
+processor.process_numbers()
